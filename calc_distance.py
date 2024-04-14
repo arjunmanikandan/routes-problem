24bc477ce43140cfda3c69217ed21871957dc513
arjunmanikandan <arjunmanikandan319@gmail.com>
2024-04-14 21:42:58 +0530

    Calc routes using csv files

--- calc_distance.py
+++ calc_distance.py
@@ -1,31 +1,48 @@
-import json
 from tabulate import tabulate
 import sys
+import csv
 
 def get_distances():
-    with open(r"C:\Users\ADMIN\Videos\Python_basics\routes_problem\distance.json","r") as file:
-        result = json.load(file)
-    return result
+    distances = []
+    with open(r"C:\Users\ADMIN\Videos\Python_basics\routes_problem\distance.csv","r") as file:
+        result = csv.reader(file)
+        for item in result:
+            distances.append(item)
+    return distances
 
 def get_routes():
-    with open(r"C:\Users\ADMIN\Videos\Python_basics\routes_problem\routes.json","r") as file:
-        result = json.load(file)
-    return result
+    routes = []
+    with open(r"C:\Users\ADMIN\Videos\Python_basics\routes_problem\routes.csv","r") as file:
+        result = csv.reader(file)
+        for item in result:
+            routes.append(item)
+    return routes
 
 def get_stops_between_cities(cities,source,destination):
-    for item in cities:
-        if item["from"] == source and item["to"] == destination or item["to"] == source and item["from"] == destination:
-            stops = item["stops"]
-            all_stops = stops.split(" ")
-    return all_stops
+    for value in range(1,len(cities)):
+        if source in cities[value][0] and destination in cities[value][0]:
+            stops = [cities[value][1]]
+    return stops
+
+def sub_lists(city,distance):
+    if city.issubset(distance):
+        result = int([item for item in list(distance) if item.isdigit()==True ][0])
+        return result
+    return 0
 
 def get_distance_between_stops(city_list,distances):
-    total_distance = 0
-    for item in distances:
-        for i in range(len(city_list)):
-            if i < len(city_list) - 1:
-                if city_list[i] == item["from"] and city_list[i+1] == item["to"] or city_list[i] == item["to"] and city_list[i+1] == item["from"]:
-                    total_distance += item["distance"]
+    result = city_list[0].split(",")
+    i,sub_list,total_distance = 0,[],0
+    for item in result:
+        if i < len(result)-1:   # len=3  we want 0,1
+            sub_list.append([result[i],result[i+1]])
+            i+=1
+    city_list = [set(item) for item in sub_list]
+    cities_distances = [set(item) for item in distances]
+    for item in city_list:
+        for value in cities_distances:
+            result = sub_lists(item,value)
+            total_distance+=result
     return total_distance
 
 def print_result(list1):
@@ -33,19 +50,12 @@ def print_result(list1):
 
 def display_distance_table(result3,start_city,end_city):
     result1 = get_stops_between_cities(result3,start_city,end_city)
-    if result1 == [""]:
-        result = get_distances()
-        for item in result:
-            if start_city == item["from"] and end_city == item["to"] or start_city == item["to"] and end_city == item["from"] :
-                distance = item["distance"]
-        print_result([[start_city,end_city,[],distance]])
-    else:
-        total_distance = get_distance_between_stops(result1,get_distances())
-        print_result([[start_city,end_city,result1,total_distance]])
+    total_distance = get_distance_between_stops(result1,get_distances())
+    print_result([[start_city,end_city,result1,total_distance]])
 
-get_cities_cli = sys.argv
-start_city = get_cities_cli[1]
-end_city = get_cities_cli[2]
-print(start_city," ",end_city)
+# get_cities_cli = sys.argv
+# start_city = get_cities_cli[1]
+# end_city = get_cities_cli[2]
+# print(start_city," ",end_city)
 
-display_distance_table(get_routes(),start_city,end_city)
+display_distance_table(get_routes(),"Nagpur","Bangalore")
